bc6907ba41981d31c287138352bcfa5cedf7a853
ElianaMT <eliana_taco@estudante.sesisenai.org.br>
2023-08-28 00:26:13 -0500

    Ex 2 - Tela de listagem de produtos

--- resolucao-ex-sem-12/carrito-de-compras/src/App.vue
+++ resolucao-ex-sem-12/carrito-de-compras/src/App.vue
@@ -1,4 +1,4 @@
 <template>
-  <v-card>222</v-card>
+  
 <router-view></router-view>
 </template>
--- resolucao-ex-sem-12/carrito-de-compras/src/views/Home/Home.vue
+++ resolucao-ex-sem-12/carrito-de-compras/src/views/Home/Home.vue
@@ -1,3 +1,72 @@
 <template>
-    Home
+    <h1>Produtos</h1>
+    <div class="list-products" >
+
+    <v-card width="30%" class="pa-2 mb-2" v-for="product in products" :key="product.id">
+        <v-img 
+        :src="product.imagem"
+            class="align-end" 
+            width="100%" 
+            hidden="200px" 
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" 
+            cover aspect-ratio="1">
+
+            <v-card-title class="text-white">{{product.nome}}</v-card-title>
+        </v-img>
+
+        <v-card-subtitle class="pt-4">
+            10X de {{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(product.parcela)}}
+        </v-card-subtitle>
+
+        <v-card-actions>
+            <v-btn color="orange">Comprar</v-btn>
+        </v-card-actions>
+    </v-card>
+
+  </div>
 </template>
+
+<script>
+import axios from "axios"
+
+export default{
+    data() {
+        return {
+            products:[ ]
+        }
+    },
+
+
+    mounted() {
+        this.loadProducts()
+        
+    },
+    methods: {
+        loadProducts() {
+            axios({
+                url: " http://localhost:3000/produtos",
+                method: "GET"
+            })
+
+        .then((response)=>{
+            this.products = response.data
+           
+        })
+
+        .catch(()=>{
+            alert("Desculpe, nao foi possivel recuper os produtos")
+
+})
+    }
+    },
+ }
+</script>
+
+<style>
+.list-products{ 
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;   
+   
+}
+</style>
